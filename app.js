2643f0fb8e11e1c7869f852f13137f2ec1dfd933
Jake Feeney <33157949+VampireSausageTech@users.noreply.github.com>
2018-07-03 14:45:07 +0100

    Efficency update
    This code is now more efficient

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/reviews', (req,res) => {
 
 app.get('/news', (req,res) => {
 	request
-	.get('http://localhost:8080/api/news?name='+req.query.Name)
+	.get('http://localhost:8080/api/news/Title?name='+req.query.Name)
 	.on('data', (data) => {
 		results = JSON.parse(data)
 		console.log("Rex Sneeze")
@@ -36,9 +36,8 @@ app.get('/news', (req,res) => {
 	})	
 });
 app.get("/test", (req,res) => {
-	let name = req.query.Name;
 	request
-	.get('http://localhost:8080/api/news?name='+req.query.Name)
+	.get('http://localhost:8080/api/news/id?name='+req.query.Name)
 	.on('data', (data) => {
 		data2 = JSON.parse(data)
 		console.log("Rex Sneeze")
@@ -46,6 +45,18 @@ app.get("/test", (req,res) => {
 		res.render("test.html", {'data2': data2})
 })
 });
+
+app.get('/api/news/id', (req,res) =>{
+	let name = req.query.name;
+	let ffs = [];
+	db.find({_id: name}, function (err,docs) {
+		var data = docs
+		console.log(data)
+		res.status(200).json(docs)
+		})
+		console.log(ffs)
+		
+		});
 
 app.get('/api/news', (req,res) =>{
 	let name = req.query.name;
@@ -64,6 +75,21 @@ app.get('/api/news', (req,res) =>{
 		res.status(200).json(ffs)
 		});
 })
+
+app.get('/api/news/title', (req,res) =>{
+	let name = req.query.name;
+	if (!name) {
+		name = '';
+	}
+	var data = new RegExp(name)
+	console.log(data);
+	db.find({title: data}, function (err,docs) {
+		console.log(docs)
+		res.status(200).json(docs)
+		});
+		});
+
+
 app.get('*', function(req, res) {
     res.redirect('/html/error.html');
 });
